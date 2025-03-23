2f3fd741a9ccb804dcd9d5c1e3f1286f00e5cc0e
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-23 21:21:44 +0300

    using reduced

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,18 +1,20 @@
-require ( './helpers.js' );
+require("./helpers.js");
 
-const sinon = require( 'sinon' )
+const sinon = require("sinon");
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
 
-describe('reducer', function() {
-  describe('batteries', function() {
-    it('should have a `totalBatteries` variable', function() {
+describe("reducer", function () {
+  describe("batteries", function () {
+    it("should have a `totalBatteries` variable", function () {
       expect(totalBatteries).to.exist;
     });
 
-    it('should have a number as a result', function() {
-      expect(totalBatteries).to.be.a('number');
+    it("should have a number as a result", function () {
+      expect(totalBatteries).to.be.a("number");
     });
 
-    it('should have made the sum of all the assembled batteries', function() {
+    it("should have made the sum of all the assembled batteries", function () {
       expect(totalBatteries).to.eql(31);
     });
   });
